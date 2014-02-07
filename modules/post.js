82694fda909757b916c407a1a5333a1bffb5ac68
root <root@AY131223205734505c21Z.(none)>
2014-02-07 17:58:24 +0800

    add mongodb

---
+++ modules/post.js
@@ -0,0 +1,92 @@
+var mongodb = require('./db');
+function Post(name, title, post){
+    this.name = name;
+    this.title = title;
+    this.post = post;
+}
+
+module.exports = Post;
+
+Post.prototype.save = function(callback) {
+    var date = new Date();
+    var time = {
+      date: date,
+      year : date.getFullYear(),
+      month : date.getFullYear() + "-" + (date.getMonth() + 1),
+      day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+      minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) 
+    }
+
+    var post = {
+        name: this.name,
+        time: time,
+        title: this.title,
+        post: this.post
+    }
+
+    mongodb.open(function(err, db){
+        if(err) {
+            mongodb.close();
+            return callback(err);
+        }
+        db.collection('posts',function(err,collection){
+            if(err){
+                return callback(err);
+            }
+            collection.insert(post,{safe:true},function(err){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                } else {
+                    callback(null);
+                }
+            });
+        });
+
+    });
+};
+Post.get = function(name, callback){
+    mongodb.open(function(err, db){
+        if(err) {
+            return callback(err);
+        }
+        db.collection('posts',function(err, collection){
+            if(err) {
+                mongdb.close();
+                return callback(err);
+            }
+            var query = {};
+            if(name) {
+                query.name = name;
+            }
+            collection.find(query).sort({time:-1}).toArray(function(err,docs){
+                mongdb.close();
+                if(err){
+                    return callback(err);
+                }
+                else {
+                    return callback(null,docs);
+                }
+            });
+        });
+    });
+};
+Post.remove = function(id, callback){
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);
+        }
+        db.collection('posts',function(err, collection){
+            if(err) {
+                mongdb.close();
+                return callback(err);
+            }
+            collection.remove({_id:id},function(err,db){
+                if(err){
+                    return callback(err);
+                }
+                return callback(null);
+            });
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Love Blog' });
-};
+var Post = require('../modules/post.js');
+module.exports = function(app) {
+    app.get('/',function(req,res){
+        res.render('index', {
+                            title: 'LoveBlog',
+                            success:req.flash('success').toString(),
+                            error:req.flash('success').toString()
+                            });
+    });
+    app.post('/',function(req,res){
+        post = new Post('fork', req.body.title, req.body.post);
+        post.save(function(err){
+        	if(err){
+        		req.flash('error',err);
+        		return res.redirect('/');
+        	}
+        	req.flash('success','success!')
+        	res.redirect('/');
+        });
+    });
+}
