78a8a6d56af25e7ecaec47d63a204865bb1cbe41
root <root@AY131223205734505c21Z.(none)>
2014-02-14 15:06:00 +0800

    qingrenjie version

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var Post = require('../modules/post.js');
 module.exports = function(app) {
     app.get('/',function(req,res){
-        res.render('index', {
+        Post.get(null, function(err,posts){
+            if(err){
+                throw(err);
+            }
+            console.log(posts);
+            res.render('index', {
                             title: 'LoveBlog',
                             success:req.flash('success').toString(),
-                            error:req.flash('success').toString()
+                            error:req.flash('success').toString(),
+                            posts:posts
                             });
+        });
     });
     app.post('/',function(req,res){
-        post = new Post('fork', req.body.title, req.body.post);
+        post = new Post(req.body.user, req.body.title, req.body.post);
+        console.log('post!!',req.body);
         post.save(function(err){
         	if(err){
         		req.flash('error',err);
